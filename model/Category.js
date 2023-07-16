15334117ce5e1f1f4ab243d21a8ff06560c430e9
iman-gholami <iman.noc1998@gmail.com>
2023-07-17 00:14:39 +0430

    Update category.js Model

--- model/Category.js
+++ model/Category.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose") ;
 
 
 const categorySchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true ,
-        unique : true ,
-    }
-})
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+});
+
+
+
 
 
 
